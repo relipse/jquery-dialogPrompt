4fa4acb8eda2e004d398aa6b60912dd4382f5dfe
relipse <relipse@gmail.com>
2013-05-17 00:23:35 -0400

    Update jquery.dialogPrompt.js

--- jquery.dialogPrompt.js
+++ jquery.dialogPrompt.js
@@ -14,13 +14,14 @@
       ok_text = ok_text || 'Ok';
       cancel_text = cancel_txt || 'Cancel';
       var counter = dlg_counter;
-      var id = '#dlgPrompt_'+counter;
+      var dlg_id = 'dlgPrompt_'+counter';
+      var ipt_id = dlg_id + '_ipt';
       
       var s = '<div id="dlgPrompt_'+counter+'" title="'+title+'"><form>';
       if (msg){
          s += '<label>'+msg; 
       }
-      s += ' <input type="'+type+'" name="dlgPrompt_ipt_'+counter+'" value="'+default_value+'" id="dlgPrompt_ipt_'+counter+'" class="text ui-widget-content ui-corner-all" />';
+      s += ' <input id="'+ipt_id+'" type="'+type+'" value="'+default_value+'"  class="text ui-widget-content ui-corner-all" />';
       if (msg){
          s += '</label>';
       }
@@ -30,7 +31,7 @@
       var dlg_buttons = {};
       var ok_function = function(){
          //close dialog unless user returns false
-         if (typeof(callback) != 'function' || callback(true, $('dlgPrompt_ipt_'+counter).val()) !== false){
+         if (typeof(callback) != 'function' || callback(true, $('#'+ipt_id).val()) !== false){
              $(this).dialog('close');
              //now free memory
              $('dlgPrompt_ipt_'+counter).dialog('destroy').remove();
@@ -40,16 +41,16 @@
       dlg_buttons[ok_text] = ok_function;
       dlg_buttons[cancel_text] = function(){
         //close dialog unless user returns false
-        if (typeof(callback) != 'function' || callback(true, $('dlgPrompt_ipt_'+counter).val()) !== false){
-             $(id).dialog('close');
+        if (typeof(callback) != 'function' || callback(true, $('#'+ipt_id).val()) !== false){
+             $('#'+dlg_id).dialog('close');
              //now free memory
              $('dlgPrompt_ipt_'+counter).dialog('destroy').remove();
          }          
       }
       
       
-      var dlg$ = $(id);
-      var frm$ = $(id + ' form');
+      var dlg$ = $('#'+dlg_id);
+      var frm$ = $('#'+dlg_id + ' form');
       
       //allow form submission (which means user pressed enter most of the time) also return OK
       frm$.submit(function(){
@@ -63,7 +64,7 @@
         buttons: dlg_buttons
       });
       
-      return {dlg:dlg$, ipt: $('dlgPrompt_ipt_'+counter), counter: counter};
+      return {dlg:dlg$, ipt: $('#'+ipt_id), counter: counter};
     }
     
     
