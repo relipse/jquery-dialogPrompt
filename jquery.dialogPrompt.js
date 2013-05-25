9cfe6e98e64220e3d7b4482e91fad6e333f5cf6b
relipse <relipse@gmail.com>
2013-05-24 22:08:11 -0400

    Update jquery.dialogPrompt.js

--- jquery.dialogPrompt.js
+++ jquery.dialogPrompt.js
@@ -6,7 +6,7 @@
  * $.dialogPrompt('Enter your name: ', '', function(value){ ... });
  *
  * @see http://github.com/relipse/jquery-dialogPrompt
- * @version 0.955
+ * @version 0.96
  */
 (function ($) {
     var dlg_counter = 1;
